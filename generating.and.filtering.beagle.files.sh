353cefe7663feabe28fc35b045ac2fa95814ba36
Fernando Hernandez <127550236+fernandoh33@users.noreply.github.com>
2026-09-04 12:39:39 -0600

    Update generating.and.filtering.beagle.files.sh

--- generating.and.filtering.beagle.files.sh
+++ generating.and.filtering.beagle.files.sh
@@ -10,8 +10,9 @@ module load angsd/0.940
 
 REF=reference/PO1735_Microstegium_vimineum.only.23.pseudochr.fasta
 #generate beagle files, i.e., snp calling in the angsd way
+filters='-doMajorMinor 1 -doPost 1 -doGlf 2 -doMaf 1 -baq 1 -minMapQ 30 -minQ 20 -SNP_pval 1e-6 -minMaf 0.01 -only_proper_pairs 1 -remove_bads 1'
 for chr in $(cat stiltgrass.chrs);
-do angsd -bam bams.vimineum -fai $REF.fai -ref $REF -nInd 431 -minInd 130 -doMajorMinor 1 -doPost 1 -doMaf 1 -doGlf 2 -out geno.for.ld/$chr.vimineum.for.ld.maf0.1.minInd0.5 -gl 2 -minMapQ 30 -minQ 20 -minMaf 0.1 -SNP_pval 1e-6 -nThreads 16 -baq 1 -only_proper_pairs 1 -remove_bads 1 -r $chr:;
+do angsd -bam bams.vimineum -fai $REF.fai -ref $REF -nInd 431 -minInd 130 $filters -out geno.for.ld/$chr.vimineum.for.ld.maf0.1.minInd0.5 -gl 2 -nThreads 16 -r $chr:;
 done;
 
 #further filtering of beagle files
